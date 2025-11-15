72ad112f9451995b2fe6690956fbdbc68212d0a3
W Gabriel <gabrielborgesw2@gmail.com>
2025-11-15 06:52:56 -0300

    first

--- src/Arrays/ListaImport.java
+++ src/Arrays/ListaImport.java
@@ -1,4 +1,25 @@
 package Arrays;
 
 public class ListaImport {
+    // criar varios arrays de tipos diferentes e eu listas para treinar importação
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};
+        String[] letras = {"A", "B", "C", "D", "E"};
+        String[] paises = {"Brasil", "Argentina", "Chile", "Peru", "Colombia"};
+
+        System.out.println("Números:");
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+
+        System.out.println("Letras:");
+        for (String letra : letras) {
+            System.out.println(letra);
+        }
+
+        System.out.println("Países:");
+        for (String pais : paises) {
+            System.out.println(pais);
+        }
+    }
 }
--- src/Arvore/ExercicioArvoreUM.java
+++ src/Arvore/ExercicioArvoreUM.java
@@ -1,4 +1,8 @@
 package Arvore;
 
 public class ExercicioArvoreUM {
+    public static void main(String[] args) {
+        // importa a lista de arrays e montar arvore com os dados
+        Arrays.ListaImport.main(args);
+    }
 }
